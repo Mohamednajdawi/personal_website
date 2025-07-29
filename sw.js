8ae84f4037ddd962ba8158f8911c8caa225f6ee8
mohammad.alnajdawi <mohammad.alnajdawi@teamviewer.com>
2025-07-29 15:33:26 +0200

    mohammad_alnajdawi.jpg

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const CACHE_NAME = "mohammad-portfolio-v1";
 const urlsToCache = [
   "/",
   "/index.html",
-  	"/assets/images/profile_pic.jpg",
+  	"/assets/images/mohammad_alnajdawi.jpg",
   "https://cdn.tailwindcss.com",
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
 ];
